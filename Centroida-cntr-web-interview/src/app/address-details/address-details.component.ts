29de02afc7c003333d9eb9538dc2cb0f32f8da22
dhtj <dhtj@abv.bg>
2018-07-01 20:36:31 +0200

    Set the new addresses to local storage.

--- Centroida-cntr-web-interview/src/app/address-details/address-details.component.ts
+++ Centroida-cntr-web-interview/src/app/address-details/address-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AddressService } from '../address.service';
 import { Address } from '../models/address';
+import {s} from "@angular/core/src/render3";
 
 @Component({
   selector: 'app-address-details',
@@ -12,6 +13,10 @@ export class AddressDetailsComponent implements OnInit {
 
   address: Address;
   addressId: number;
+  fullName: string;
+  city: string;
+  zipCode: string;
+  street: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,8 +29,28 @@ export class AddressDetailsComponent implements OnInit {
   }
 
   save() {
+    if (this.verifyLocalStorage()){
+      let currentExternalAddresses = JSON.parse(localStorage.getItem('myExternalAddresses'));
+      if (!currentExternalAddresses) {
+        currentExternalAddresses = {
+          addresses: []
+        };
+      }
+      currentExternalAddresses.addresses.push({
+        name: this.fullName,
+        id: 'local' + currentExternalAddresses.addresses.length,
+        address: {
+          street: this.street,
+          city: this.city,
+          zipCode: this.zipCode,
+          suite: 'not-available'
+        }
+      });
+      localStorage.setItem('myExternalAddresses',JSON.stringify(currentExternalAddresses));
 
-
+    } else {
+      alert('Local Storage Not Available. Aborting!');
+    }
     this.router.navigate(['']);
   }
 
@@ -33,4 +58,14 @@ export class AddressDetailsComponent implements OnInit {
     this.router.navigate(['']);
   }
 
+  verifyLocalStorage () {
+    const test = 'testLocalStorage';
+    try {
+      localStorage.setItem(test, test);
+      localStorage.removeItem(test);
+      return true;
+    } catch(ex) {
+      return false;
+    }
+  }
 }
