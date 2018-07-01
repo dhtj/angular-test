cd933eeec5ddafc903665fab3bdeaa820f57398a
dhtj <dhtj@abv.bg>
2018-07-01 19:10:21 +0200

    Implement routing to AddressDetailsComponent.

--- Centroida-cntr-web-interview/src/app/app-routing.module.ts
+++ Centroida-cntr-web-interview/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { AddressDetailsComponent } from './address-details/address-details.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: 'address', component: AddressDetailsComponent }
 ];
 
 @NgModule({
